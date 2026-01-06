d548d93bf3347e24cd60210c390adfb6a97ac405
aggoldenking-sudo <aggoldenking@gmail.com>
2026-01-06 15:07:00 -0400

    Add Firebase configuration and initialization

---
+++ js/firebase.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+  apiKey: "TU_API_KEY",
+  authDomain: "TU_PROYECTO.firebaseapp.com",
+  projectId: "TU_PROYECTO",
+  storageBucket: "TU_PROYECTO.appspot.com",
+  messagingSenderId: "TU_ID",
+  appId: "TU_APP_ID"
+};
+
+// Inicializar Firebase
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const db = firebase.firestore();
